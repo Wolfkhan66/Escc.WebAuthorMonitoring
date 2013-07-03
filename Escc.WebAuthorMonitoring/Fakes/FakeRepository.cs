fcb7be85faedb67a21a65634e81ce08805607e93
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-07-03 15:26:25 +0100

    Add new files belonging to previous commit

---
+++ Escc.WebAuthorMonitoring/Fakes/FakeRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+namespace Escc.WebAuthorMonitoring.Fakes
+{
+    /// <summary>
+    /// Fake repository for web author monitoring
+    /// </summary>
+    public class FakeRepository : IWebAuthorMonitoringRepository
+    {
+        /// <summary>
+        /// Reads the possible types of problem which could be reported.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<ProblemType> ReadProblemTypes()
+        {
+            return new List<ProblemType>()
+                {
+                    new ProblemType(){ ProblemTypeId=1, Name="Urgent problem", DefaultText = "We've noticed an urgent problem."},
+                    new ProblemType() {ProblemTypeId=2, Name = "Common problem", DefaultText = "We've noticed a common problem."},
+                    new ProblemType(){ProblemTypeId=3,Name = "Minor problem", DefaultText = "We've noticed a minor problem."}
+                };
+        }
+    }
+}
---
+++ Escc.WebAuthorMonitoring/ProblemType.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Escc.WebAuthorMonitoring
+{
+    /// <summary>
+    /// A type of problem which can be reported to responsible web authors
+    /// </summary>
+    public class ProblemType
+    {
+        /// <summary>
+        /// Gets or sets the problem type id.
+        /// </summary>
+        /// <value>The problem type id.</value>
+        public int ProblemTypeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default text.
+        /// </summary>
+        /// <value>The default text.</value>
+        public string DefaultText { get; set; }
+    }
+}
